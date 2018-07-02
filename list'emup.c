89381b7accd22b8d30724e0eb7fbe43286e63451
anirbanchowdhury9862 <animesh.n78@gmail.com>
2018-07-02 15:07:05 +0530

    Code indented.

--- list'emup.c
+++ list'emup.c
@@ -8,150 +8,177 @@
 #include <unistd.h>
 #include <time.h>
 
-int compare (const FTSENT**, const FTSENT**);
-int sizecmp( const FTSENT *,const FTSENT *);
-int namecmp( const FTSENT *,const FTSENT *);
-int acccmp( const FTSENT *,const FTSENT *);
-int statcmp( const FTSENT *,const FTSENT *);
-int modcmp( const FTSENT *,const FTSENT *);
-int (*sortfnc)(const FTSENT *,const FTSENT *);
+int compare(const FTSENT **, const FTSENT **);
+int sizecmp(const FTSENT *, const FTSENT *);
+int namecmp(const FTSENT *, const FTSENT *);
+int acccmp(const FTSENT *, const FTSENT *);
+int statcmp(const FTSENT *, const FTSENT *);
+int modcmp(const FTSENT *, const FTSENT *);
+int (*sortfnc) (const FTSENT *, const FTSENT *);
 
-int main(int argc, char* const argv[])
-{   int  choice,i=0;
-    int f_namesort;
-    int f_sizesort;
-    int f_accesstimesort;
-    int f_statuschangesort;
-    int f_modificationsort;
-    char ch[128];
+int main(int argc, char *const argv[])
+{
+	int choice, i = 0;
+	int f_namesort;
+	int f_sizesort;
+	int f_accesstimesort;
+	int f_statuschangesort;
+	int f_modificationsort;
+	char ch[128];
 
+	FTS *file_system = NULL;
+	FTSENT *parent = NULL;
 
-    FTS* file_system = NULL;
-    FTSENT* parent = NULL;
+	if (argc < 1) {
+		printf("Usage: %s <path-spec>\n", argv[0]);
+		exit(255);
+	}
 
-    if (argc<1)
-    {
-        printf("Usage: %s <path-spec>\n", argv[0]);
-        exit(255);
-    }
+	do {
+		i = 0;
+		printf
+		    ("enter your choice of sorting\n1.namesort\n2.sizesort\n3.accesstimesort\n4.statuschangetimesort\n5.modificationtimesort\n");
+		scanf("%d", &choice);
+		switch (choice) {
 
+		case 1:
+			sortfnc = namecmp;
+			f_namesort = 1;
+			f_sizesort = 0;
+			f_accesstimesort = 0;
+			f_statuschangesort = 0;
+			f_modificationsort = 0;
+			break;
+		case 2:
+			sortfnc = sizecmp;
+			f_namesort = 0;
+			f_sizesort = 1;
+			f_accesstimesort = 0;
+			f_statuschangesort = 0;
+			f_modificationsort = 0;
 
+			break;
+		case 3:
+			sortfnc = acccmp;
+			f_namesort = 0;
+			f_sizesort = 0;
+			f_accesstimesort = 1;
+			f_statuschangesort = 0;
+			f_modificationsort = 0;
 
-    do{
-    i=0;
-    printf("enter your choice of sorting\n1.namesort\n2.sizesort\n3.accesstimesort\n4.statuschangetimesort\n5.modificationtimesort\n");
-    scanf("%d",&choice);
-    switch(choice){
+			break;
+		case 4:
+			sortfnc = statcmp;
+			f_namesort = 0;
+			f_sizesort = 0;
+			f_accesstimesort = 0;
+			f_statuschangesort = 1;
+			f_modificationsort = 0;
 
-    case 1:sortfnc=namecmp;
-                f_namesort        =1;
-                f_sizesort        =0;
-                f_accesstimesort  =0;
-                f_statuschangesort=0;
-                f_modificationsort=0;
-                break;
-    case 2:sortfnc=sizecmp;
-                f_namesort        =0;
-                f_sizesort        =1;
-                f_accesstimesort  =0;
-                f_statuschangesort=0;
-                f_modificationsort=0;
+			break;
+		case 5:
+			sortfnc = modcmp;
+			f_namesort = 0;
+			f_sizesort = 0;
+			f_accesstimesort = 0;
+			f_statuschangesort = 0;
+			f_modificationsort = 1;
 
-                break;
-    case 3:sortfnc=acccmp;
-                f_namesort        =0;
-                f_sizesort        =0;
-                f_accesstimesort  =1;
-                f_statuschangesort=0;
-                f_modificationsort=0;
+			break;
+		default:
+			printf("\nwe assume you want to exit");
+			exit(1);
+			break;
 
-                break;
-    case 4:sortfnc=statcmp;
-                f_namesort        =0;
-                f_sizesort        =0;
-                f_accesstimesort  =0;
-                f_statuschangesort=1;
-                f_modificationsort=0;
+		}
 
-                break;
-    case 5:sortfnc=modcmp;
-                f_namesort        =0;
-                f_sizesort        =0;
-                f_accesstimesort  =0;
-                f_statuschangesort=0;
-                f_modificationsort=1;
+		if (argc != 2) {
+			strcpy(argv[0], getcwd(ch, 128));
 
-                break;
-    default:printf("\nwe assume you want to exit");
-                exit(1);
-                break;
+			file_system =
+			    fts_open(argv + 0, FTS_PHYSICAL | FTS_XDEV,
+				     &compare);
+		}
 
+		else {
+			file_system =
+			    fts_open(argv + 1, FTS_PHYSICAL | FTS_XDEV,
+				     &compare);
+		}
 
-    }
+		if (NULL != file_system) {
+			while ((parent = fts_read(file_system)) != NULL) {
+				if (i == 0) {
+					strcpy(ch, parent->fts_name);
+					i++;
+				}
+				if (strcmp(ch, parent->fts_name) != 0) {
+					fts_set(file_system, parent, FTS_SKIP);
+					if (f_namesort)
+						printf("\n%llu\t\t%s",
+						       parent->fts_statp->
+						       st_size,
+						       parent->fts_name);
 
+					if (f_statuschangesort)
+						printf("\n%s\t\t%s",
+						       ctime(&parent->
+							     fts_statp->
+							     st_ctime),
+						       parent->fts_name);
 
-if(argc!=2)
-       {strcpy(argv[0],getcwd(ch,128));
+					if (f_modificationsort)
+						printf("\n%s\t\t%s",
+						       ctime(&parent->
+							     fts_statp->
+							     st_mtime),
+						       parent->fts_name);
 
-        file_system = fts_open(argv+0,FTS_PHYSICAL|FTS_XDEV,&compare);
-        }
+					if (f_accesstimesort)
+						printf("\n%s\t\t%s",
+						       ctime(&parent->
+							     fts_statp->
+							     st_atime),
+						       parent->fts_name);
 
-else
-    {file_system = fts_open(argv + 1,FTS_PHYSICAL|FTS_XDEV,&compare);
-    }
+					if (f_sizesort)
+						printf("\n%llu\t\t%s",
+						       parent->fts_statp->
+						       st_size,
+						       parent->fts_name);
 
-    if (NULL != file_system)
-    {while( ( parent=fts_read(file_system)) != NULL)
-        {  if(i==0)
-            {strcpy(ch,parent->fts_name);
-                i++;
-                }
-                if(strcmp(ch,parent->fts_name)!=0)
-                 { fts_set(file_system,parent,FTS_SKIP);
-                    if(f_namesort)
-                    printf("\n%llu\t\t%s",parent->fts_statp->st_size,parent->fts_name);
+				}
 
-                    if(f_statuschangesort)
-                    printf("\n%s\t\t%s",ctime(&parent->fts_statp->st_ctime),parent->fts_name);
+			}
 
-                    if(f_modificationsort)
-                    printf("\n%s\t\t%s",ctime(&parent->fts_statp->st_mtime),parent->fts_name);
-
-                    if(f_accesstimesort)
-                    printf("\n%s\t\t%s",ctime(&parent->fts_statp->st_atime),parent->fts_name);
-
-                    if(f_sizesort)
-                    printf("\n%llu\t\t%s",parent->fts_statp->st_size,parent->fts_name);
-
-
-
-                    }
-
-                }
-
-        fts_close(file_system);
-    }}while(1);
-    return 0;
+			fts_close(file_system);
+		}
+	} while (1);
+	return 0;
 }
 
-int compare(const FTSENT** one, const FTSENT** two)
-{ return (*sortfnc)(*one,*two);
+int compare(const FTSENT ** one, const FTSENT ** two)
+{
+	return (*sortfnc) (*one, *two);
 }
-int sizecmp(const FTSENT *a,const FTSENT *b){
 
-if(a->fts_statp->st_size>b->fts_statp->st_size)
-   return 1;
-else
-   if(a->fts_statp->st_size<b->fts_statp->st_size)
-    return -1;
-    else
-      return namecmp(a,b);
+int sizecmp(const FTSENT * a, const FTSENT * b)
+{
+
+	if (a->fts_statp->st_size > b->fts_statp->st_size)
+		return 1;
+	else if (a->fts_statp->st_size < b->fts_statp->st_size)
+		return -1;
+	else
+		return namecmp(a, b);
 }
-int namecmp(const FTSENT *a, const FTSENT *b)
+
+int namecmp(const FTSENT * a, const FTSENT * b)
 {
 	return (strcoll(a->fts_name, b->fts_name));
 }
-int acccmp(const FTSENT *a, const FTSENT *b)
+
+int acccmp(const FTSENT * a, const FTSENT * b)
 {
 	if (b->fts_statp->st_atime > a->fts_statp->st_atime)
 		return (1);
@@ -161,7 +188,8 @@ int acccmp(const FTSENT *a, const FTSENT *b)
 	else
 		return (namecmp(a, b));
 }
-int statcmp(const FTSENT *a, const FTSENT *b)
+
+int statcmp(const FTSENT * a, const FTSENT * b)
 {
 	if (b->fts_statp->st_ctime > a->fts_statp->st_ctime)
 		return (1);
@@ -172,7 +200,8 @@ int statcmp(const FTSENT *a, const FTSENT *b)
 		return (namecmp(a, b));
 
 }
-int modcmp(const FTSENT *a, const FTSENT *b)
+
+int modcmp(const FTSENT * a, const FTSENT * b)
 {
 	if (b->fts_statp->st_mtime > a->fts_statp->st_mtime)
 		return (1);
@@ -182,4 +211,3 @@ int modcmp(const FTSENT *a, const FTSENT *b)
 	else
 		return (namecmp(a, b));
 }
-
